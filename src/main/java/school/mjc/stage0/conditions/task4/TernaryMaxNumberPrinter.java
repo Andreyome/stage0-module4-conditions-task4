6c71288077c1845d4837b79fb9f0eeb53007f3ac
Andreyome <andreyomem@gmail.com>
2022-12-07 18:14:49 +0200

    commit 2

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-        System.out.println(first>=second&&first>=third?first: second>=third&&second>=first?second:third);
+        int result=first>=second&&first>=third?first: second>=third&&second>=first?second:third;
+System.out.println(result);
     }
 }
